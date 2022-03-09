4b4d652c56f0e7f1af9aba72823f3b7dabe08b97
Kamal S <muha.kamal@gmail.com>
2022-03-09 10:36:57 +0700

    add option to collapse or uncollapse in grader

--- grader/src/grader.py
+++ grader/src/grader.py
@@ -6,6 +6,7 @@ from grader.src.constants import Constants
 from grader.src.ged.classes.cost_function import CostFunction
 from grader.src.ged.classes.general_cost_function import GeneralCostFunction
 from grader.src.ged.dfs_ged import DFSGED
+from grader.src.ged.utils.graph_collapser import collapse, uncollapse
 
 
 def grade_one_on_one(graph_source: Graph, graph_target: Graph, cost_function: CostFunction=None) -> float:
@@ -19,11 +20,21 @@ def grade_one_on_one(graph_source: Graph, graph_target: Graph, cost_function: Co
     return score * Constants.MAX_SCORE
 
 
-def grade(graph_source: Graph, graph_targets: list[Graph]):
+def grade(graph_source: Graph, graph_targets: list[Graph], collapse_graph: bool = True):
+    if collapse_graph:
+        graph_source = collapse(graph_source)
+    else:
+        graph_source = uncollapse(graph_source)
+
     cost_function = GeneralCostFunction()
     scores = []
 
-    for graph_target in graph_targets:
+    for rgraph_target in graph_targets:
+        graph_target = rgraph_target
+        if collapse_graph:
+            graph_target = collapse(rgraph_target)
+        else:
+            graph_target = uncollapse(rgraph_target)
         scores.append(grade_one_on_one(graph_source, graph_target, cost_function))
 
     return scores
